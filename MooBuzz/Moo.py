cfef650f4945212b973860fad4b47ffe507f0b60
AnmolS1Z <71851700+AnmolS1Z@users.noreply.github.com>
2020-10-28 00:51:19 -0500

    Update Moo.py

--- MooBuzz/Moo.py
+++ MooBuzz/Moo.py
@@ -1,5 +1,8 @@
 N = int(input('Enter a number: '))
 num_list = [1, 2, 4, 7, 8, 11, 13, 14]
 num = N%8-1
-answer = num_list[num]+(15*(N//8))
+if num != -1:
+    answer = num_list[num] + (15 * (N // 8))
+else:
+    answer = num_list[num] + (15 * (N // 8 - 1))
 print(answer)
